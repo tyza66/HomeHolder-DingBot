7f867df004fccfd1554cd2f4ef833027ec4aa7bf
洮羱芝闇 <shun_@outlook.com>
2023-11-23 23:04:51 +0800

    Update HomeControl.py

--- 服务程序/python/HomeControl.py
+++ 服务程序/python/HomeControl.py
@@ -1,42 +1,36 @@
 import requests
 import os
 
-hard_server = os.environ.get('HARD_SERVER', '')
-location_code = os.environ.get('LOCATION_CODE', '')
+hard_server = os.environ.get('HARD_SERVER', '192.168.100.106')
+location_code = os.environ.get('LOCATION_CODE', '101010100')
 hefeng_key = os.environ.get('HEFENG_KEY', '')
 
 #温度
 def get_temperature():
     try:
         response = requests.get("http://"+hard_server + '/wd')
-        response.encoding = 'utf-8'
+        response = "当前室内温度为：" + response.text + "℃"
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 #湿度
 def get_humidity():
     try:
         response = requests.get("http://" + hard_server + '/sd')
-        response.encoding = 'utf-8'
+        response = "当前室内湿度为：" + response.text + "（空气电阻）"
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 #光照
 def get_light():
     try:
         response = requests.get("http://" + hard_server + '/ld')
-        response.encoding = 'utf-8'
+        response = "当前室内光照情况为：" + response.text
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 #全部
 def get_all():
@@ -46,51 +40,42 @@ def get_all():
 def one_open():
     try:
         response = requests.get("http://" + hard_server + '/wo')
-        response.encoding = 'utf-8'
+        response = response.text
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 #用电器关
 def one_close():
     try:
         response = requests.get("http://" + hard_server + '/wc')
-        response.encoding = 'utf-8'
+        response = response.text
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 #灯开
 def light_open():
     try:
         response = requests.get("http://" + hard_server + '/kd')
-        response.encoding = 'utf-8'
+        response = "灯已开启"
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 #灯关
 def light_close():
     try:
         response = requests.get("http://" + hard_server + '/gd')
-        response.encoding = 'utf-8'
+        response = "灯已关闭"
     except:
         response = "出错了，检查主控硬件是否正常"
-        return response
-    print(response)
-    return "ok"
+    return response
 
 # 获得当前天气预报
 def get_hefeng():
     try:
         response = requests.get("https://devapi.qweather.com/v7/weather/now?location="+location_code+"&key="+hefeng_key)
-        print(response.json())
         markdown = "总览信息：" + response.json()['now']['text'] + "，" + response.json()['now']['temp'] + "℃，" + response.json()['now']['windDir'] + response.json()['now']['windScale'] + "级  \n"
         markdown += "体感温度：" + response.json()['now']['feelsLike'] + "℃  \n"
         markdown += "相对湿度：" + response.json()['now']['humidity'] + "%  \n"
